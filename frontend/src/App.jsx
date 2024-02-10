bfe68206085073626aa3eec29ce841f690a427f6
Angela <aplozano2004@gmail.com>
2024-02-10 12:38:38 -0500

    Setup React development environment with Vite and installed essential dependencies (Tailwind CSS, React Router Dom, Zustand, and Tanstack Query) as per documentation.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-blue-500">Hello World</h1>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/main.jsx
@@ -0,0 +1,20 @@
+import './styles/index.css'
+
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+// Create a client
+const queryClient = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
+)
